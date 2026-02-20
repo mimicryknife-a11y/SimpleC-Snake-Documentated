92f3ca5044388607ec0565bc54f4208e4bd32cf7
adelramz1230-design <adelramz1230@gmail.com>
2026-02-20 19:44:20 +0500

    моя часть документации

--- SimpleC++Snake/platform_commands.h
+++ SimpleC++Snake/platform_commands.h
@@ -4,13 +4,22 @@
 #include "utilities.h"
 #include "renderer.h"
 #include "object_manager.h"
-
+/**
+*	@brief Ввод 
+*/
 struct Input;
-
+/**
+*	@brief положение кнопки 
+*	@params is_down проверка на нажатие
+*	@params changed проверка на измение я хз
+*/
 struct ButtonState {
 	bool is_down = false;
 	bool changed = false;
 };
+/**
+*	@brief список кнопок 
+*/
 
 enum { // holds every button action in the game
 	BUTTON_MOVEUP,
@@ -21,13 +30,23 @@ enum { // holds every button action in the game
 	// how many buttons actions there are in total
 	BUTTON_COUNT 
 };
-
+/**
+*	@brief Ввод
+*/
 struct Input {
 	std::array<ButtonState, BUTTON_COUNT> buttons;
 };
-
+/**
+*	@brief запрашивает положение кнопок  
+*/
 void getKeyStates(uint32& vk_code, bool& is_down, HWND* window);
+/**
+*	@brief В ДУШЕ НЕ ЧАЮ ЧЕС СЛОВО 
+*/
 void inputProcessing(uint32 vk_code, bool is_down);
+/**
+*	@brief читайте бриф выше 
+*/
 void updateVelocityFromInputs(PlayerObject& player);
 
 extern Input inputs;
--- SimpleC++Snake/renderer.h
+++ SimpleC++Snake/renderer.h
@@ -10,19 +10,47 @@ extern float startYPercent;
 
 extern int rendererMin;
 extern int rendererMax;
-
+/**
+* @brief рендеринг заднего фона
+*/
 void renderBackground();
-
+/*
+*	@brief  обновляет размер экрана
+*/
 void updateVars();
-
+/*
+	@brief рендерит клетку
+	@params tilemap  клетка карты 
+*/
 void renderTilemap(std::array<std::array<uint32, tilemapSizeY>, tilemapSizeX>& tilemap);
-
+/*
+	@brief отображает счет в конце игры
+	@params score счет 
+	@params hWnd настолько очевидно что я даже обьяснять не буду
+*/
 void renderScore(HWND* hWnd, int score);
-
+/*
+	@brief очищает экран
+	@params color цвет хз
+*/
 void clearScreen(uint32 color);
-
+/*
+* @brief рисует пиксели мб
+* @params color читайте выще
+* @params  x0 координата 0x
+* @params  y0 координата 0y
+* @params  x1 координата x1
+* @params  y1 координата y1
+*/
 void drawRectInPixels(int x0, int y0, int x1, int y1, uint32 color);
-
+/*
+* @brief рисует пиксели мб
+* @params color читайте выще
+* @params  x0 координата 0x
+* @params  y0 координата 0y
+* @params  x1 координата x1
+* @params  y1 координата y1
+*/
 void drawRectInPercent(float x0, float y0, float x1, float y1, uint32 color);
 
 #endif
